5768d82534fd3c913230d5f2364bc730d14b12ad
Даниял Ибраев <daniyal.ibrayev@MacBook-Pro-Danial.local>
2024-01-21 23:40:34 +0600

    Opp2

---
+++ src/Person.java
@@ -0,0 +1,60 @@
+public class Person implements Payable, Comparable<Person>{
+    protected static int nextId = 1;
+
+    protected int id;
+    private String name;
+    private String surname;
+
+    @Override
+    public String toString(){
+        return id+"."+name+" "+surname;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public String getSurname() {
+        return surname;
+    }
+
+    public void setSurname(String surname) {
+        this.surname = surname;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+
+    public Person() {
+        this.id = generateId();
+    }
+
+    public Person(String name, String surname) {
+        this.name = name;
+        this.surname = surname;
+    }
+
+    public String getPosition(){
+        return "Student";
+    }
+
+    private static int generateId() {
+        return nextId++;
+    }
+
+    @Override
+    public double getPaymentAmount() {
+        return 0;
+    }
+
+    @Override
+    public int compareTo(Person o) {
+        return  Double.compare(this.getPaymentAmount(), o.getPaymentAmount());
+    }
+}
